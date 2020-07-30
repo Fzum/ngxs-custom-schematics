69714aabb959d184eaafcfca92eb84fe16174946
Fabian Sellemond <fabian.sellemond@wienit.at>
2020-07-30 14:00:05 +0200

    fix

--- src/ngxs-schematics/files/__name@dasherize__/__name@dasherize__.view.component.ts
+++ src/ngxs-schematics/files/__name@dasherize__/__name@dasherize__.view.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-<%= dasherize(name) %>-view',
-    templateUrl: './<%= dasherize(name) %>.component.html',
-    styleUrls: ['./<%= dasherize(name) %>.component.scss'],
+    templateUrl: './<%= dasherize(name) %>.view.component.html',
+    styleUrls: ['./<%= dasherize(name) %>.view.component.scss'],
 })
 export class <%= classify(name) %>ViewComponent implements OnInit {
     @Input() input: any;
