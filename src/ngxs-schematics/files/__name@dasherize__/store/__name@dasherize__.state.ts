d03a0c6a36798c9bf524d7f944405c7f14e9726c
Fabian Sellemond <fabian.sellemond@wienit.at>
2020-08-26 15:15:01 +0200

    add comment

--- src/ngxs-schematics/files/__name@dasherize__/store/__name@dasherize__.state.ts
+++ src/ngxs-schematics/files/__name@dasherize__/store/__name@dasherize__.state.ts
@@ -2,6 +2,9 @@ import { State, StateContext, Selector } from '@ngxs/store';
 import { <%= classify(name) %>ActionPayload } from './<%= dasherize(name) %>.actions';
 import { EmitterAction, Receiver } from '@ngxs-labs/emitter';
 
+// todo: if this state should be persisted (survive f5 and browser exit),
+// todo: add it to NgxsStoragePluginModule.forRoot([..., <%= classify(name) %>State])
+
 export interface <%= classify(name) %>StateModel {
   stuff: string;
 }
